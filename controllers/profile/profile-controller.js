e81e33ddafc29bfc5cb60a9eee9baf1ec164c9fa
Hafees S <hafees15hafi@gmail.com>
2023-07-25 17:01:14 +0530

    Created Apis - getProfile - changeDp - followUser - unfollowUser

--- controllers/profile/profile-controller.js
+++ controllers/profile/profile-controller.js
@@ -1,17 +1,134 @@
+import multer from 'multer'
+import fs from 'fs'
+import cloudinary from 'cloudinary'
+import { CREDENTIALS, IMAGE_FORMATS, STORAGE } from '../../constants/constants.js'
+import Posts from '../../schema/post/post-schema.js'
+import Users from '../../schema/user/user-schema.js'
+import { storage } from '../../storage/storage.multer.js'
+
 export class ProfileController {
-    static getProfile(req, res) {
-        res.status(200).json({ "User Profile": req.params.username })
+
+    // get any users profile data
+    static async getProfile(req, res) {
+        if (!req.params.username) {
+            return res.status(400).json({ error: "User not found" })
+        }
+        Users.findOne({ username: req.params.username }).then((user) => {
+            if (user) {
+                const { username, dp, followers, following, bio } = user
+                if (!username, dp, followers, following, bio) {
+                    return res.status(400).json({ error: "User not found" })
+                }
+                Posts.find({ postedBy: user._id }).then((posts) => {
+                    res.json({
+                        userposts: posts,
+                        user: {
+                            username,
+                            dp,
+                            userId: user._id,
+                            followers,
+                            following,
+                            bio: bio == undefined || null ? "" : bio
+                        }
+                    })
+                })
+            } else {
+                return res.status(400).json({ error: "User not found" })
+            }
+        })
     }
+
+
+    // Changing dp of user
     static changeDp(req, res) {
-        res.send("DP Changed")
+        const upload = multer({ storage: storage(STORAGE.DP) }).single('dp');
+
+        upload(req, res, (err) => {
+            if (err) {
+                console.log(err)
+                return res.status(400).json({ error: "Upload Failed" })
+            } else {
+                const path = STORAGE.DP;
+                const oldPath = path + req.file.filename;
+                const newPath = path + req.user._id + IMAGE_FORMATS.JPG;
+
+                fs.rename(oldPath, newPath, (err, done) => {
+                    if (!err)
+                        cloudinary.uploader.upload(newPath, {
+                            api_key: CREDENTIALS.CLOUDINARY_API,
+                            api_secret: CREDENTIALS.CLOUDINARY_SECRET,
+                            cloud_name: CREDENTIALS.CLOUD_NAME,
+                            upload_preset: CREDENTIALS.UPLOAD_PRESET
+                        },
+                            (error, result) => {
+                                if (error) {
+                                    return res.json({ error: "Error changing dp" })
+                                }
+                                fs.unlink(newPath,  (err)=> {
+                                    if (err) {
+                                        return res.json({ error: "Error changing dp" })
+                                    }
+                                });
+                                Users.findByIdAndUpdate(req.user._id,
+                                    { "dp": result.url },
+                                    { new: true }).then((resultDb) => {
+                                        res.json({ message: "Successfully Uploaded" })
+                                    }).catch(err => { console.log(err) })
+                            })
+
+                })
+            }
+        })
+    };
+
+    static async followUser(req, res) {
+        if (!req.body.userId) {
+            return res.status(400).json({ error: "Error following user" })
+        }
+        Users.findByIdAndUpdate(req.body.userId,
+            { $push: { followers: req.user._id } },
+            { new: true },
+            (error, result) => {
+                if (error) {
+                    res.json({ error: error })
+                } else {
+                    Users.findByIdAndUpdate(req.user._id,
+                        { $push: { following: req.body.userId } },
+                        { new: true }
+                    ).then((newresult) => {
+                        res.json({ result: result, newresult: newresult })
+                    }).catch((err) => {
+                        return res.status(400).json({ error: err })
+                    })
+                }
+            }
+        )
+
     }
 
-    static followUser(req, res) {
-        res.send("followed")
+    static async unfollowUser(req, res) {
+        if (!req.body.userId) {
+            return res.status(400).json({ error: "Error while unfollowing user" })
+        }
+        Users.findByIdAndUpdate(req.body.userId,
+            { $pull: { followers: req.user._id } },
+            { new: true },
+            (error, result) => {
+                if (error) {
+                    return res.status(400).json({ error: "Error while unfollowing user" })
+                } else {
+                    Users.findByIdAndUpdate(req.user._id,
+                        { $pull: { following: req.body.userId } },
+                        { new: true }
+                    ).then((newresult) => {
+                        res.json({ result: result, newresult: newresult })
+                    }).catch((err) => {
+                        return res.status(400).json({ error: "Error while unfollowing user" })
+                    })
+                }
+            }
+        )
+
     }
 
-    static unfollowUser(req, res) {
-        res.send("Unfollowed")
-    }
-    
 }
--- routes/profile/profile-routes.js
+++ routes/profile/profile-routes.js
@@ -1,15 +1,16 @@
 import express from 'express'
 import { ENDPOINTS } from '../../constants/constants.js'
 import { ProfileController } from '../../controllers/profile/profile-controller.js'
+import requireLogin from '../../middlewares/require-login.js'
 
-const profileRouter=express.Router()
+const profileRouter = express.Router()
 
-profileRouter.get(ENDPOINTS.PROFILE,ProfileController.getProfile)
+profileRouter.get(ENDPOINTS.PROFILE, requireLogin, ProfileController.getProfile)
 
-profileRouter.put(ENDPOINTS.CHANGEDP,ProfileController.changeDp)
+profileRouter.put(ENDPOINTS.CHANGEDP, requireLogin, ProfileController.changeDp)
 
-profileRouter.put(ENDPOINTS.FOLLOW, ProfileController.followUser)
+profileRouter.put(ENDPOINTS.FOLLOW, requireLogin, ProfileController.followUser)
 
-profileRouter.put(ENDPOINTS.UNFOLLOW, ProfileController.unfollowUser)
+profileRouter.put(ENDPOINTS.UNFOLLOW, requireLogin, ProfileController.unfollowUser)
 
 export default profileRouter
